676e106b9e098db854c51a4a79b73d80bd4332d6
naoyashiga <naoyashiga0@gmail.com>
2016-09-01 08:15:29 +0900

    add comment out

--- demo/components/particle/ParticleSystem.js
+++ demo/components/particle/ParticleSystem.js
@@ -6,19 +6,19 @@ export default class ParticleSystem {
     this.vertices = [];
     this.velThetaArr = [];
     this.velRadArr = [];
-    this.velocities = [];
+    // this.velocities = [];
     this.thetaArr = [];
-    this.freqArr = [];
-    this.boldRateArr = [];
+    // this.freqArr = [];
+    // this.boldRateArr = [];
 
-    this.randomTargetXArr = []
-    this.randomTargetYArr = []
+    // this.randomTargetXArr = []
+    // this.randomTargetYArr = []
 
     this.particles = []
 
-    this.numLines = 1000
+    this.numLines = 4000
 
-    this.count = 0
+    // this.count = 0
   }
 
   addParticle() {
@@ -35,17 +35,17 @@ export default class ParticleSystem {
       this.thetaArr.push(p.theta);
       this.velThetaArr.push(p.velTheta);
       this.velRadArr.push(p.radius);
-      this.freqArr.push(p.freq);
-      this.boldRateArr.push(p.boldRate);
+      // this.freqArr.push(p.freq);
+      // this.boldRateArr.push(p.boldRate);
 
 
-      this.randomTargetXArr.push(p.randomPosX);
-      this.randomTargetYArr.push(p.randomPosY);
+      // this.randomTargetXArr.push(p.randomPosX);
+      // this.randomTargetYArr.push(p.randomPosY);
     })
 
-    this.freqArr = new Float32Array(this.freqArr);
+    // this.freqArr = new Float32Array(this.freqArr);
     this.vertices = new Float32Array(this.vertices)
-    this.velocities = new Float32Array(this.velocities)
+    // this.velocities = new Float32Array(this.velocities)
 
     this.thetaArr = new Float32Array(this.thetaArr)
     this.velThetaArr = new Float32Array(this.velThetaArr)
@@ -60,32 +60,35 @@ export default class ParticleSystem {
     var num;
     var targetX, targetY;
 
-    for (i = 0; i < this.numLines * 2; i += 2) {
-      this.count += .3;
+    for (let i = 0; i < this.numLines * 2; i += 2) {
+      // this.count += .3;
       bp = i * 3;
 
-      this.vertices[bp] = this.vertices[bp + 3];
-      this.vertices[bp + 1] = this.vertices[bp + 4];
+      this.vertices[bp] = this.vertices[bp + 3]
+      this.vertices[bp + 1] = this.vertices[bp + 4]
 
-      num = parseInt(i / 2);
-      pTheta = this.thetaArr[num];
-      rad = this.velRadArr[num];
+      num = parseInt(i / 2)
+      pTheta = this.thetaArr[num]
+      rad = this.velRadArr[num]
 
-      pTheta = pTheta + this.velThetaArr[num];
-      this.thetaArr[num] = pTheta;
+      this.vertices[bp] = rad * Math.cos(pTheta)
+      this.vertices[bp + 1] = rad * Math.sin(pTheta)
 
-      targetX = rad * Math.cos(pTheta);
-      targetY = rad * Math.sin(pTheta);
+      pTheta = pTheta + this.velThetaArr[num]
+      this.thetaArr[num] = pTheta
 
-      px = this.vertices[bp + 3];
-      px += (targetX - px) * (Math.random() * .1 + .1);
-      this.vertices[bp + 3] = px;
+      targetX = rad * Math.cos(pTheta)
+      targetY = rad * Math.sin(pTheta)
+
+      px = this.vertices[bp + 3]
+      px += (targetX - px) * (Math.random() * .1 + .1)
+      this.vertices[bp + 3] = px
 
 
       // py = (Math.sin(cn) + 1) * .2 * (Math.random() * .5 - .25);
-      py = this.vertices[bp + 4];
-      py += (targetY - py) * (Math.random() * .1 + .1);
-      this.vertices[bp + 4] = py;
+      py = this.vertices[bp + 4]
+      py += (targetY - py) * (Math.random() * .1 + .1)
+      this.vertices[bp + 4] = py
     }
   }
 }
